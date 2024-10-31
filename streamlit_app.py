e150e39928f99efd4999fa3374eb1885d9b2247c
Junbo Koh <89024993+Ridealist@users.noreply.github.com>
2024-10-31 10:39:32 +0000

    첫 웹사이트 개꿀

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.subheader("예비교원 워크숍 10월 특강 실습 페이지")
